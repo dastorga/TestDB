c0fe9137de2ab804965f0dbc38b086381a6b1166
Dario <astorgadm@gmail.com>
2017-09-27 18:27:41 -0300

    Changes in MainActivity

--- app/src/main/java/com/example/dario/testsdb/CrearActivity.java
+++ app/src/main/java/com/example/dario/testsdb/CrearActivity.java
@@ -1,13 +1,20 @@
 package com.example.dario.testsdb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class CrearActivity extends AppCompatActivity {
+
+    EditText etNombreGrafo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear);
+
+        etNombreGrafo = (EditText) findViewById(R.id.etNombreGrafo);
+
+
     }
 }
