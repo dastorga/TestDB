ef9d461a59f7f8cdb508452521f4d592f83cc89a
Dario <astorgadm@gmail.com>
2017-09-27 13:38:42 -0300

    Add activity_main.xml (btnEliminar)

--- app/src/main/java/com/example/dario/testsdb/MainActivity.java
+++ app/src/main/java/com/example/dario/testsdb/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.dario.testsdb;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCrear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button btnCrear = (Button) findViewById(R.id.btnCrear);
+
         final MiBaseDatos MDB = new MiBaseDatos(getApplicationContext());
+
+
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+
+            }
+        });
+
+
 
 
     }
