45fa79bad7298af9be2b7cfae6ec4b4f29e9eea4
Dario <astorgadm@gmail.com>
2017-09-27 11:42:22 -0300

    Add Enlace, Graph, Node, DB

--- app/src/main/java/com/example/dario/testsdb/MainActivity.java
+++ app/src/main/java/com/example/dario/testsdb/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.dario.testsdb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        final MiBaseDatos MDB = new MiBaseDatos(getApplicationContext());
+
+    }
 }
