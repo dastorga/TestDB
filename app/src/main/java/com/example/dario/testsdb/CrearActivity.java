c6b13b7c6994b33350f331717068b6214b5a7d15
Dario <astorgadm@gmail.com>
2017-09-28 11:00:51 -0300

    Add Buttons in View Crear

--- app/src/main/java/com/example/dario/testsdb/CrearActivity.java
+++ app/src/main/java/com/example/dario/testsdb/CrearActivity.java
@@ -2,14 +2,14 @@ package com.example.dario.testsdb;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class CrearActivity extends AppCompatActivity {
 
     EditText etNombreGrafo;
-    Button btnGuardar,btnEditar;
+    Button btnGuardar,btnEditar,btnVolverAtras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +19,39 @@ public class CrearActivity extends AppCompatActivity {
         etNombreGrafo = (EditText) findViewById(R.id.etNombreGrafo);
         btnGuardar = (Button) findViewById(R.id.btnGuardar);
         btnEditar = (Button) findViewById(R.id.btnEditar);
+        btnVolverAtras = (Button) findViewById(R.id.btnVolverAtras);
+
         final MiBaseDatos MDB = new MiBaseDatos(getApplicationContext());
 
-        Toast.makeText(getApplicationContext(), "Estoy en CREAR", Toast.LENGTH_SHORT).show();
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
 
+//                String nombreGrafo = etNombreGrafo.getText().toString();
+//                MDB.insertGraph(1,nombreGrafo);
+//
+//                Toast.makeText(getApplicationContext(), "GUARDADO", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+            }
+        });
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+//                Toast.makeText(getApplicationContext(), MDB.recoverGraph(1).getNameGraph(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btnVolverAtras.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+//                Toast.makeText(getApplicationContext(), "ATRAS", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/dario/testsdb/MainActivity.java
+++ app/src/main/java/com/example/dario/testsdb/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), CrearActivity.class));
 
 
-
             }
         });
 
