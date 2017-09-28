4bda339764c2b8c5c7fb7670900a0ed3c6d8fcb0
Dario <astorgadm@gmail.com>
2017-09-28 10:49:31 -0300

    Add Buttons in View Crear

--- app/src/main/java/com/example/dario/testsdb/CrearActivity.java
+++ app/src/main/java/com/example/dario/testsdb/CrearActivity.java
@@ -2,12 +2,14 @@ package com.example.dario.testsdb;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class CrearActivity extends AppCompatActivity {
 
     EditText etNombreGrafo;
+    Button btnGuardar,btnEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +17,12 @@ public class CrearActivity extends AppCompatActivity {
         setContentView(R.layout.activity_crear);
 
         etNombreGrafo = (EditText) findViewById(R.id.etNombreGrafo);
+        btnGuardar = (Button) findViewById(R.id.btnGuardar);
+        btnEditar = (Button) findViewById(R.id.btnEditar);
+        final MiBaseDatos MDB = new MiBaseDatos(getApplicationContext());
 
         Toast.makeText(getApplicationContext(), "Estoy en CREAR", Toast.LENGTH_SHORT).show();
-        final MiBaseDatos MDB = new MiBaseDatos(getApplicationContext());
+
 
 
     }
