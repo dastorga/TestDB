d2af748c891b6cbe3f4e79eb20f54fb44dede0d8
Dario <astorgadm@gmail.com>
2017-11-13 17:03:24 -0300

    Add EditarNodoGrafo class

--- app/src/main/java/com/example/dario/testsdb/EditarNodoGrafo.java
+++ app/src/main/java/com/example/dario/testsdb/EditarNodoGrafo.java
@@ -1,6 +1,5 @@
 package com.example.dario.testsdb;
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,6 +8,8 @@ public class EditarNodoGrafo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Aqui debere listar todos los nodos y solicitar uno para editarlo, nombre y atributo
 
     }
 }
