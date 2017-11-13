9da2be27a69c357c8450de33c02dc2ab9c52698e
Dario <astorgadm@gmail.com>
2017-11-13 10:14:43 -0300

    Add GraphList and Adapter in graph

---
+++ app/src/main/java/com/example/dario/testsdb/GraphList.java
@@ -0,0 +1,8 @@
+package com.example.dario.testsdb;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class GraphList extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/com/example/dario/testsdb/GraphListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.dario.testsdb;
+
+/**
+ * Created by dario on 13/11/17.
+ */
+
+public class GraphListAdapter {
+}
