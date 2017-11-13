89efc2cc944d7c427bbbf5e931fe313c969df98e
Dario <astorgadm@gmail.com>
2017-11-13 17:43:11 -0300

    EditarNodoGrafo...

--- app/src/main/java/com/example/dario/testsdb/EditarNodoGrafo.java
+++ app/src/main/java/com/example/dario/testsdb/EditarNodoGrafo.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
 public class EditarNodoGrafo extends AppCompatActivity {
