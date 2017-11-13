f76d76438d824b31bdeda7993e336e5686b6d362
Dario <astorgadm@gmail.com>
2017-11-13 17:00:21 -0300

    Add EditarNodoGrafo class

---
+++ app/src/main/java/com/example/dario/testsdb/EditarNodoGrafo.java
@@ -0,0 +1,14 @@
+package com.example.dario.testsdb;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditarNodoGrafo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
