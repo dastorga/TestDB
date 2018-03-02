b74ab6410f1af07633c5970a5df6e7f2fcad9c8e
Dario <astorgadm@gmail.com>
2018-03-01 22:08:34 -0300

    Add EditarEnlace class

---
+++ app/src/main/java/com/example/dario/testsdb/EditarEnlace.java
@@ -0,0 +1,12 @@
+package com.example.dario.testsdb;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dario on 1/3/18.
+ * Encargada de editar(Atributo, nodo Origen y nodo Destino) del enlace seleccionado.
+ */
+
+public class EditarEnlace extends AppCompatActivity {
+
+}
