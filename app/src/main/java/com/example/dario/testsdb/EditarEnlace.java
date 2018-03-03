cd2b8ea620791f4a44789d8c45dc98622be2f184
Dario <astorgadm@gmail.com>
2018-03-03 12:07:24 -0300

    Add RelativeLayout in layout

--- app/src/main/java/com/example/dario/testsdb/EditarEnlace.java
+++ app/src/main/java/com/example/dario/testsdb/EditarEnlace.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by dario on 1/3/18.
@@ -13,30 +14,32 @@ import android.widget.Button;
 
 public class EditarEnlace extends AppCompatActivity {
 
-    Button button_editar_atributo_enlace, button_editar_nodo_origen_enlace, button_editar_nodo_destino_enlace,
+    Button button_editar_nodo_origen_enlace, button_editar_nodo_destino_enlace,
     button_editar_guardar_enlace, button_editar_volver_atras_enlace;
-
+    EditText et_editar_atributo_enlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_enlace);
 
-        button_editar_atributo_enlace = findViewById(R.id.button_editar_atributo_enlace);
+        et_editar_atributo_enlace = findViewById(R.id.et_editar_atributo_enlace);
         button_editar_nodo_origen_enlace = findViewById(R.id.button_editar_nodo_origen_enlace);
         button_editar_nodo_destino_enlace = findViewById(R.id.button_editar_nodo_destino_enlace);
         button_editar_guardar_enlace = findViewById(R.id.button_editar_guardar_enlace);
         button_editar_volver_atras_enlace = findViewById(R.id.button_editar_volver_atras_enlace);
 
-
         /**
          * Cambiar valor de atributo
          */
-        button_editar_atributo_enlace.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        final MiBaseDatos MDB = new MiBaseDatos(getApplicationContext());
+        Globals g = Globals.getInstance();
+        final int idEnlaceEdit = g.getIdEnlaceEdit(); // recupero el id del enlace a editar
+        final int idGlobalEdit = g.getIdGlobalEdit(); // recupero el id del grafo a editar
 
-            }
-        });
+        // pongo el atributo actual del enlace, en el campo a editar
+        et_editar_atributo_enlace.setText(MDB.recoverEnlace(idEnlaceEdit).getAtributoEnlace());
+
 
         /**
          * Cambiar nodo Origen
