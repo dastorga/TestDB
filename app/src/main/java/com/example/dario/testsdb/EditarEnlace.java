4114350439316b0bf3a27ecef69bde4443da394b
Dario <astorgadm@gmail.com>
2018-03-03 11:20:25 -0300

    Updates EditarEnlace class

--- app/src/main/java/com/example/dario/testsdb/EditarEnlace.java
+++ app/src/main/java/com/example/dario/testsdb/EditarEnlace.java
@@ -1,6 +1,10 @@
 package com.example.dario.testsdb;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by dario on 1/3/18.
@@ -8,5 +12,69 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class EditarEnlace extends AppCompatActivity {
+
+    Button button_editar_atributo_enlace, button_editar_nodo_origen_enlace, button_editar_nodo_destino_enlace,
+    button_editar_guardar_enlace, button_editar_volver_atras_enlace;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_enlace);
+
+        button_editar_atributo_enlace = findViewById(R.id.button_editar_atributo_enlace);
+        button_editar_nodo_origen_enlace = findViewById(R.id.button_editar_nodo_origen_enlace);
+        button_editar_nodo_destino_enlace = findViewById(R.id.button_editar_nodo_destino_enlace);
+        button_editar_guardar_enlace = findViewById(R.id.button_editar_guardar_enlace);
+        button_editar_volver_atras_enlace = findViewById(R.id.button_editar_volver_atras_enlace);
+
+
+        /**
+         * Cambiar valor de atributo
+         */
+        button_editar_atributo_enlace.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        /**
+         * Cambiar nodo Origen
+         */
+        button_editar_nodo_origen_enlace.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        /**
+         * Cambiar nodo Destino
+         */
+        button_editar_nodo_destino_enlace.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        /**
+         * Guardar cambios
+         */
+        button_editar_guardar_enlace.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        /**
+         * Volver hacia atras
+         */
+        button_editar_volver_atras_enlace.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GraphListEdit.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
 }
