c77965bb595d33b2db3e3b89e9022e6e2b1a622d
Dario <astorgadm@gmail.com>
2018-05-02 22:55:56 -0300

    Update in export

--- app/src/main/java/com/example/dario/testsdb/WhatsAppExport.java
+++ app/src/main/java/com/example/dario/testsdb/WhatsAppExport.java
@@ -29,7 +29,7 @@ public class WhatsAppExport extends AppCompatActivity {
         editText_telefono_whatsapp = findViewById(R.id.editText_telefono_whatsapp);
         btn_enviar_whatsapp = findViewById(R.id.btn_enviar_whatsapp);
 
-        final String number = String.valueOf(editText_telefono_whatsapp.getText()); // obtengo el numero que se ingreso, se valida como type phone
+        //final String number = String.valueOf(editText_telefono_whatsapp.getText()); // obtengo el numero que se ingreso, se valida como type phone
                                              // luego aqui yo lo paso a text.
 
         final Globals g = Globals.getInstance();
@@ -51,30 +51,31 @@ public class WhatsAppExport extends AppCompatActivity {
 
                 c = new ConstructionGraph(getApplicationContext()); // Paso contexto actual
                 String constructionGraphString = c.ConstructionGraphString(idGlobalPlay); // paso id del grafo a construir
-//                Toast.makeText(WhatsAppExport.this, constructionGraphString  , Toast.LENGTH_SHORT).show();
-//                Log.i("------------------->", constructionGraphString);
+                //Toast.makeText(WhatsAppExport.this, constructionGraphString  , Toast.LENGTH_SHORT).show();
+                //Log.i("------------------->", constructionGraphString);
 
                 // Me abre WhatsApp directamente
                 //Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.whatsapp");
                 //startActivity(launchIntent);
 
-                if(number == ""){
-                    // Opcion en la que no inserta ningun numero
+                if(editText_telefono_whatsapp.getText().toString().trim().equalsIgnoreCase("")){
+                    // Opcion en la que no inserta ningun numero, el campo esta vacio.
+                    // Se abre el panel de opciones para enviar donde nos paresca mejor
+                    // por email, por WhatsApp, etc.
+                    Toast.makeText(WhatsAppExport.this, "No has introducido numero", Toast.LENGTH_SHORT).show();
                     Intent sendIntent = new Intent();
                     sendIntent.setAction(Intent.ACTION_SEND);
-                    sendIntent.putExtra(Intent.EXTRA_TEXT, "http://graphviz.it/#/new"+constructionGraphString);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, "Ingresa a: https://dreampuf.github.io/GraphvizOnline/ y pega el siguiente grafo:"+"\n"+constructionGraphString);
+                    //sendIntent.putExtra(Intent.EXTRA_TEXT, constructionGraphString);
                     sendIntent.setType("text/plain");
                     startActivity(sendIntent);
                 } else {
-                    // Opcion en la que inserta numero de telefono
-                    Toast.makeText(WhatsAppExport.this, number, Toast.LENGTH_SHORT).show();
-
+                    // Opcion en la que inserta numero de telefono, se busca el contacto en WhatsApp y se le envia el mensaje.
+                    //Toast.makeText(WhatsAppExport.this, editText_telefono_whatsapp.getText().toString(), Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(Intent.ACTION_VIEW);
-                    String uri = "whatsapp://send?phone=" + number + "&text=" + constructionGraphString;
+                    String uri = "whatsapp://send?phone=" + editText_telefono_whatsapp.getText().toString() + "&text=" +"http://graphviz.it/#/"+ constructionGraphString;
                     intent.setData(Uri.parse(uri));
                     startActivity(intent);
-
-                    Toast.makeText(WhatsAppExport.this, "Enviado exitosamente!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
