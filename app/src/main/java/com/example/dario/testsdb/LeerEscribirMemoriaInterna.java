8069cbf206a982736bc6c30d06dab8eb7419a637
Dario <astorgadm@gmail.com>
2018-05-06 18:41:38 -0300

    Save in Memory Internal

--- app/src/main/java/com/example/dario/testsdb/LeerEscribirMemoriaInterna.java
+++ app/src/main/java/com/example/dario/testsdb/LeerEscribirMemoriaInterna.java
@@ -1,146 +1,38 @@
 package com.example.dario.testsdb;
 
-import android.content.Context;
-import android.os.Bundle;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 /**
  * Created by dario on 6/5/18.
  */
 
-public class LeerEscribirMemoriaInterna extends AppCompatActivity {
+public class LeerEscribirMemoriaInterna  {
 
-    boolean sdDisponible = false;
-    boolean sdAccesoEscritura = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Código que me comprueba si existe SD y si puedo escribir o no
-        String estado = Environment.getExternalStorageState();
-
-        if (estado.equals(Environment.MEDIA_MOUNTED))
-        {
-            sdDisponible = true;
-            sdAccesoEscritura = true;
-        }
-        else if (estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY))
-        {
-            sdDisponible = true;
-            sdAccesoEscritura = false;
-        }
-        else
-        {
-            sdDisponible = false;
-            sdAccesoEscritura = false;
-        }
-
-    }
-
-    public String leermeminterna(){
-        String texto = "";
-        try
-        {
-            BufferedReader fin = new BufferedReader(new InputStreamReader(openFileInput("meminterna.txt")));
-            texto = fin.readLine();
-            Toast.makeText(LeerEscribirMemoriaInterna.this, "Leo correctamente"  , Toast.LENGTH_SHORT).show();
-            fin.close();
-        }
-        catch (Exception ex)
-        {
-            Log.e("Ficheros", "Error al leer fichero desde la memoria interna");
-        }
-        return texto;
-    }
-
-    public void escribirmeminterna(String grafo){
-        try
-        {
-            OutputStreamWriter fout= new OutputStreamWriter(openFileOutput("meminterna.txt", Context.MODE_PRIVATE));
-            fout.write(grafo); // guardo el grafo
-            Toast.makeText(LeerEscribirMemoriaInterna.this, "Escribi correctamente"  , Toast.LENGTH_SHORT).show();
-            fout.close();
-        }
-        catch (Exception ex)
-        {
-            Log.e("Ficheros", "Error al escribir fichero en la memoria interna");
-        }
-
-    }
-
-    public String leersd() {
-        String texto = "";
-        if (sdDisponible) {
-            try {
-                File ruta_sd = Environment.getExternalStorageDirectory();
-
-                File f = new File(ruta_sd.getAbsolutePath(), "ficherosd.txt");
-
-                BufferedReader fin =
-                        new BufferedReader(
-                                new InputStreamReader(
-                                        new FileInputStream(f)));
-
-                texto = fin.readLine();
-                fin.close();
-            } catch (Exception ex) {
-                Log.e("Ficheros", "Error al leer fichero desde tarjeta SD");
-            }
-        }
-        return texto;
-    }
-
-    public void escribirsd(){
-        if(sdAccesoEscritura && sdDisponible){
-            try
-            {
-                File ruta_sd = Environment.getExternalStorageDirectory();
-
-                File f = new File(ruta_sd.getAbsolutePath(), "ficherosd.txt");
-
-                OutputStreamWriter fout =
-                        new OutputStreamWriter(
-                                new FileOutputStream(f));
-
-                fout.write("Contenido del fichero de la SD");
-                fout.close();
-            }
-            catch (Exception ex)
-            {
-                Log.e("Ficheros", "Error al escribir fichero en la tarjeta SD");
-            }
-        }
-    }
-
-//    public  void leerprograma(){
-//        try
-//        {
-//            InputStream fraw = getResources().openRawResource(R.raw.ficheroraw);
+//    File internalStorageDir = getFilesDir();
+//                Toast.makeText(LeerEscribirMemoriaInterna.this, internalStorageDir + ""  , Toast.LENGTH_SHORT).show();
 //
-//            BufferedReader brin = new BufferedReader(new InputStreamReader(fraw));
+//    File alice = new File(internalStorageDir, "alice.txt");
 //
-//            String linea = brin.readLine();
-//            textView.setText(linea);
-//
-//            fraw.close();
-//        }
-//        catch (Exception ex)
-//        {
-//            Log.e("Ficheros", "Error al leer fichero desde recurso raw");
-//        }
+//    // Create file output stream
+//    FileOutputStream fos = null;
+//                try {
+//        fos = new FileOutputStream(alice);
+//    } catch (FileNotFoundException e) {
+//        e.printStackTrace();
 //    }
+//    // Write a line to the file
+//                try {
+//        fos.write(constructionGraphString.getBytes());
+//        Toast.makeText(LeerEscribirMemoriaInterna.this,  "pude escribir!!!!!!!!"  , Toast.LENGTH_SHORT).show();
+//    } catch (IOException e) {
+//        e.printStackTrace();
+//    }
+//    // Close the file output stream
+//                try {
+//        fos.close();
+//    } catch (IOException e) {
+//        e.printStackTrace();
+//    }
+
 
 }
 
--- app/src/main/java/com/example/dario/testsdb/WhatsAppExport.java
+++ app/src/main/java/com/example/dario/testsdb/WhatsAppExport.java
@@ -1,15 +1,21 @@
 package com.example.dario.testsdb;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Esta clase se encargara de exportar el grafo seleccionado con anterioridad y exportarlo al numero de WhatsApp ingresado
@@ -51,6 +57,7 @@ public class WhatsAppExport extends AppCompatActivity {
          * Tambien probar correctamente la API de WhatsApp, para chequear seguridad, velocidad, en los envios de link's.
          */
         btn_enviar_whatsapp.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             public void onClick(View v) {
                 c = new ConstructionGraph(getApplicationContext()); // Paso contexto actual
                 String constructionGraphString = c.ConstructionGraphString(idGlobalPlay); // paso id del grafo a construir
@@ -61,41 +68,57 @@ public class WhatsAppExport extends AppCompatActivity {
                 //Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.whatsapp");
                 //startActivity(launchIntent);
 
+                //-------------------- Save in Memory Internal --------------------
+                File internalStorageDir = getFilesDir();
+                File fileGraph = new File(internalStorageDir, "graph.dot");
 
+                // Create file output stream
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(fileGraph);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                // Write in the file
+                try {
+                    fos.write(constructionGraphString.getBytes());
+                    Toast.makeText(WhatsAppExport.this,  "Escritura correcta"  , Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // Close the file output stream
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //-------------------- -------------------- --------------------
 
-                LeerEscribirMemoriaInterna l = new LeerEscribirMemoriaInterna();
-                l.escribirmeminterna(constructionGraphString);
-
-
-
-
-
-
-//                if(editText_telefono_whatsapp.getText().toString().trim().equalsIgnoreCase("")){
-//                    // Opcion en la que no inserta ningun numero, el campo esta vacio.
-//                    // Se abre el panel de opciones para enviar donde nos paresca mejor
-//                    // por email, por WhatsApp, etc.
-//                    Toast.makeText(WhatsAppExport.this, "No has introducido numero", Toast.LENGTH_SHORT).show();
-//                    Intent sendIntent = new Intent();
-//                    sendIntent.setAction(Intent.ACTION_SEND);
-//                    sendIntent.putExtra(Intent.EXTRA_TEXT, "Ingresa a: https://dreampuf.github.io/GraphvizOnline/ y pega el siguiente grafo:"+"\n"+constructionGraphString);
-//                    //sendIntent.putExtra(Intent.EXTRA_TEXT, constructionGraphString);
-//                    sendIntent.setType("text/plain");
-//                    startActivity(sendIntent);
-//                } else {
-//                    // Opcion en la que inserta numero de telefono, se busca el contacto en WhatsApp y se le envia el mensaje.
-//                    //Toast.makeText(WhatsAppExport.this, editText_telefono_whatsapp.getText().toString(), Toast.LENGTH_SHORT).show();
-//                    Intent intent = new Intent(Intent.ACTION_VIEW);
-//                    String uri = "whatsapp://send?phone=" + editText_telefono_whatsapp.getText().toString() + "&text=" +"http://graphviz.it/#/"+ constructionGraphString;
-//                    intent.setData(Uri.parse(uri));
-//                    startActivity(intent);
-//                }
+                if(editText_telefono_whatsapp.getText().toString().trim().equalsIgnoreCase("")){
+                    // Opcion en la que no inserta ningun numero, el campo esta vacio.
+                    // Se abre el panel de opciones para enviar donde nos paresca mejor
+                    // por email, por WhatsApp, etc.
+                    Toast.makeText(WhatsAppExport.this, "No has introducido numero", Toast.LENGTH_SHORT).show();
+                    Intent sendIntent = new Intent();
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, "Ingresa a: https://dreampuf.github.io/GraphvizOnline/ y pega el siguiente grafo:"+"\n"+constructionGraphString);
+                    //sendIntent.putExtra(Intent.EXTRA_TEXT, constructionGraphString);
+                    sendIntent.setType("text/plain");
+                    startActivity(sendIntent);
+                } else {
+                    // Opcion en la que inserta numero de telefono, se busca el contacto en WhatsApp y se le envia el mensaje.
+                    //Toast.makeText(WhatsAppExport.this, editText_telefono_whatsapp.getText().toString(), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    String uri = "whatsapp://send?phone=" + editText_telefono_whatsapp.getText().toString() + "&text=" +"http://graphviz.it/#/"+ constructionGraphString;
+                    intent.setData(Uri.parse(uri));
+                    startActivity(intent);
+                }
             }
         });
 
 
         /**
-         * Me retorna a la activity anterior, osea el menu principal.
+         * Me retorna a la activity anterior, osea al menu principal.
          *
          */
         btn_volver_inicio.setOnClickListener(new View.OnClickListener() {
