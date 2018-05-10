9f5ab26c2d4897313281857963ffd1a9a1f6557f
Dario <astorgadm@gmail.com>
2018-05-09 23:05:22 -0300

    updates data base

--- app/src/main/java/com/example/dario/testsdb/MiBaseDatos.java
+++ app/src/main/java/com/example/dario/testsdb/MiBaseDatos.java
@@ -18,7 +18,7 @@ public class MiBaseDatos extends SQLiteOpenHelper {
     private static final int VERSION_BASEDATOS = 1;
 
     // Nombre de nuestro archivo de base de datos
-    private static final String NOMBRE_BASEDATOS = "mibasedatos11.db";
+    private static final String NOMBRE_BASEDATOS = "mibasedatos12.db";
 
     // Sentencia SQL para la creación de tabla WORD
     private static final String TABLA_WORD = "CREATE TABLE WORD" + "(_id INT PRIMARY KEY, word VARCHAR)";
